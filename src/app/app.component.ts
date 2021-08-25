ae05d921ff9f07232a976525893eb12982efb5fc
Joesh Gichon <joeshgichon56@gmail.com>
2021-08-25 21:50:53 +0300

    Goal app second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Goal } from "./goal"
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'Goals-App';
+
+  // goals!:string[];
+  // goal = 'Watch finding Nemo';
+
+  // constructor(){
+  //   this.goals = ["Watch finding Nemo","Buy new cookies","Buy a phone case"];
+  // }
+
+ 
+  
 }
